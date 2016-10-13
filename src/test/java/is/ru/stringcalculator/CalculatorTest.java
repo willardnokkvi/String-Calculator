76af6d86e761374148d519dea6c528b6318a0b12
willardnokkvi <willardnokkvi@gmail.com>
2016-10-13 20:23:27 +0000

    Test fyrir 2 numbers og multiple numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -14,4 +14,14 @@ public class CalculatorTest {
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
+
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, Calculator.add("1,2"));
+	}
+
+	@Test
+    public void testMultipleNumbers(){
+    	assertEquals(6, Calculator.add("1,2,3"));
+    }
 }
