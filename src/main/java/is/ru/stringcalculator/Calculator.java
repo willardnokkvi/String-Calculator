f1f136b6bbe23fce71ae852344b3c6d0303e2eac
willardnokkvi <willardnokkvi@gmail.com>
2016-10-16 18:00:22 +0000

    Sma fix

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -9,6 +9,9 @@ public class Calculator {
 		else if(text.contains(",")){
 				return sum(splitNumbers(text));
 		}
+		else if (text.contains("|\n")){
+			return sum(splitNumbers(text));
+		}
 		else
 			return 1;
 	}
@@ -18,7 +21,7 @@ public class Calculator {
 	}
 
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+	    return numbers.split(",|\n");
 	}
       
     private static int sum(String[] numbers){
@@ -28,7 +31,6 @@ public class Calculator {
 		}
 		return total;
     }
-
 
 
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
+	public static void main(String args[]){
+		org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+	}
+
 	@Test
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
@@ -27,7 +31,7 @@ public class CalculatorTest {
 
     @Test
     public void testNewLineNumbers(){
-    	assertEquals(6, Calculator.add("1/n2,3"));
+    	assertEquals(6, Calculator.add("1\n2,3"));
     }
 
 }
